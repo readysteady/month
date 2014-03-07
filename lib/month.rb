5832a7abf6b8939bfe5fa8231bf7165828d41041
Tim Craft <mail@timcraft.com>
2014-03-07 15:52:22 +0000

    Add initial spec/implementation

---
+++ lib/month.rb
@@ -0,0 +1,160 @@
+require 'date'
+
+class Month
+  NAMES = {
+    1 => :January,
+    2 => :February,
+    3 => :March,
+    4 => :April,
+    5 => :May,
+    6 => :June,
+    7 => :July,
+    8 => :August,
+    9 => :September,
+    10 => :October,
+    11 => :November,
+    12 => :December
+  }
+
+  def initialize(year, number)
+    unless NAMES.has_key?(number)
+      raise ArgumentError, 'invalid month number'
+    end
+
+    @year, @number = year, number
+  end
+
+  attr_reader :year, :number
+
+  def to_s
+    "#@year-#{@number.to_s.rjust(2, '0')}"
+  end
+
+  def name
+    NAMES.fetch(@number)
+  end
+
+  NAMES.each do |number, name|
+    define_method(:"#{name.downcase}?") do
+      @number == number
+    end
+  end
+
+  def hash
+    [@year, @number].hash
+  end
+
+  def eql?(object)
+    object.class == self.class && object.hash == self.hash
+  end
+
+  def <=>(month)
+    if @year == month.year
+      @number <=> month.number
+    else
+      @year <=> month.year
+    end
+  end
+
+  include Comparable
+
+  def next
+    if @number == 12
+      self.class.new(@year + 1, 1)
+    else
+      self.class.new(@year, @number + 1)
+    end
+  end
+
+  alias_method :succ, :next
+
+  def step(limit)
+    unless block_given?
+      return enum_for(:step, limit)
+    end
+
+    month = self
+
+    until month > limit
+      yield month
+
+      month = month.succ
+    end
+  end
+
+  def +(number)
+    a, b = (@number + number).divmod(12)
+
+    self.class.new(@year + a, b)
+  end
+
+  def -(number)
+    if @number > number
+      self.class.new(@year, @number - number)
+    elsif @number < number
+      a, b = (@number - number).divmod(12)
+
+      self.class.new(@year + a, b)
+    else
+      self.class.new(@year - 1, 12)
+    end
+  end
+
+  def include?(date)
+    @year == date.year && @number == date.month
+  end
+
+  alias_method :===, :include?
+
+  def start_date
+    Date.new(@year, @number, 1)
+  end
+
+  def end_date
+    Date.new(@year, @number, -1)
+  end
+
+  def dates
+    start_date .. end_date
+  end
+
+  def length
+    end_date.mday
+  end
+end
+
+def Month.parse(string)
+  if string =~ /\A(\d{4})-(\d{2})\z/
+    Month.new($1.to_i, $2.to_i)
+  else
+    raise ArgumentError, 'invalid month'
+  end
+end
+
+def Month(object)
+  case object
+  when Month
+    object
+  when Integer
+    Month(Time.at(object))
+  else
+    Month.new(object.year, object.month)
+  end
+end
+
+class Month
+  module Methods
+    NAMES.each do |number, name|
+      define_method(name) do |*args|
+        case args.length
+        when 1
+          Month.new(args.first, number)
+        when 2
+          Date.new(args[1], number, args[0])
+        else
+          raise ArgumentError, 'too many arguments'
+        end
+      end
+    end
+  end
+end
---
+++ spec/month_spec.rb
@@ -0,0 +1,238 @@
+require 'minitest/autorun'
+
+require_relative '../lib/month'
+
+describe 'Month' do
+  it 'raises an exception when initialized with an invalid number' do
+    proc { Month.new(2014, 0) }.must_raise(ArgumentError)
+    proc { Month.new(2014, 100) }.must_raise(ArgumentError)
+  end
+
+  describe 'year method' do
+    it 'returns the integer year of the month' do
+      Month.new(2014, 1).year.must_equal(2014)
+    end
+  end
+
+  describe 'number method' do
+    it 'returns the integer number of the month' do
+      Month.new(2014, 1).number.must_equal(1)
+    end
+  end
+
+  describe 'to_s method' do
+    it 'returns a string containing the year and zero padded number of the month' do
+      Month.new(2014, 1).to_s.must_equal('2014-01')
+    end
+  end
+
+  describe 'name method' do
+    it 'returns the name of the month' do
+      Month.new(2014, 1).name.must_equal(:January)
+    end
+  end
+
+  describe 'january predicate method' do
+    it 'returns true if the month is january' do
+      Month.new(2014, 1).january?.must_equal(true)
+    end
+
+    it 'returns false otherwise' do
+      Month.new(2014, 2).january?.must_equal(false)
+    end
+  end
+
+  describe 'february predicate method' do
+    it 'returns true if the month is february' do
+      Month.new(2014, 2).february?.must_equal(true)
+    end
+
+    it 'returns false otherwise' do
+      Month.new(2014, 1).february?.must_equal(false)
+    end
+  end
+
+  it 'supports being used as a hash key' do
+    hash, n = Hash.new(0), 10
+
+    n.times { hash[Month.new(2014, 1)] += 1 }
+
+    hash.count.must_equal(1)
+    hash[Month.new(2014, 1)].must_equal(n)
+  end
+
+  it 'supports comparison between month objects' do
+    Month.new(2014, 1).must_be_kind_of(Comparable)
+
+    (Month.new(2014, 1) == Month.new(2014, 1)).must_equal(true)
+    (Month.new(2014, 1) == Month.new(2014, 2)).must_equal(false)
+
+    (Month.new(2014, 1) < Month.new(2014, 2)).must_equal(true)
+    (Month.new(2014, 2) > Month.new(2014, 1)).must_equal(true)
+  end
+
+  it 'supports being used in a range' do
+    range = Month.new(2014, 1) .. Month.new(2014, 4)
+
+    range.map(&:number).must_equal([1, 2, 3, 4])
+  end
+
+  describe 'next method' do
+    it 'returns a month object denoting the next month' do
+      Month.new(2014, 1).next.must_equal(Month.new(2014, 2))
+      Month.new(2014, 12).next.must_equal(Month.new(2015, 1))
+    end
+  end
+
+  describe 'succ method' do
+    it 'returns a month object denoting the next month' do
+      Month.new(2014, 1).succ.must_equal(Month.new(2014, 2))
+      Month.new(2014, 12).succ.must_equal(Month.new(2015, 1))
+    end
+  end
+
+  describe 'step method' do
+    it 'calls the block for every month until the given limit' do
+      months = []
+
+      Month.new(2014, 1).step(Month.new(2014, 3)) { |month| months << month }
+
+      months.must_equal([Month.new(2014, 1), Month.new(2014, 2), Month.new(2014, 3)])
+    end
+
+    it 'returns an enumerator if no block was given' do
+      months = Month.new(2014, 1).step(Month.new(2014, 3))
+
+      months.must_be_instance_of(Enumerator)
+
+      months.next.must_equal(Month.new(2014, 1))
+      months.next.must_equal(Month.new(2014, 2))
+      months.next.must_equal(Month.new(2014, 3))
+    end
+  end
+
+  describe 'addition' do
+    it 'returns a month object denoting the given number of months after self' do
+      (Month.new(2014, 1) + 1).must_equal(Month.new(2014, 2))
+      (Month.new(2014, 1) + 12).must_equal(Month.new(2015, 1))
+      (Month.new(2014, 1) + 18).must_equal(Month.new(2015, 7))
+    end
+  end
+
+  describe 'subtraction' do
+    it 'returns a month object denoting the given number of months before self' do
+      (Month.new(2014, 2) - 1).must_equal(Month.new(2014, 1))
+      (Month.new(2014, 1) - 1).must_equal(Month.new(2013, 12))
+      (Month.new(2014, 1) - 12).must_equal(Month.new(2013, 1))
+      (Month.new(2014, 1) - 18).must_equal(Month.new(2012, 7))
+    end
+  end
+
+  describe 'include predicate method' do
+    it 'returns true if the month includes the given date' do
+      Month.new(2014, 1).include?(Date.new(2014, 1, 1)).must_equal(true)
+    end
+
+    it 'returns false otherwise' do
+      Month.new(2014, 1).include?(Date.new(2014, 2, 1)).must_equal(false)
+    end
+  end
+
+  describe 'case equals method' do
+    it 'returns true if the month includes the given date' do
+      (Month.new(2014, 1) === Date.new(2014, 1, 1)).must_equal(true)
+    end
+
+    it 'returns false otherwise' do
+      (Month.new(2014, 1) === Date.new(2014, 2, 1)).must_equal(false)
+    end
+  end
+
+  describe 'start_date method' do
+    it 'returns a date object denoting the first day of the month' do
+      Month.new(2014, 1).start_date.must_equal(Date.new(2014, 1, 1))
+    end
+  end
+
+  describe 'end_date method' do
+    it 'returns a date object denoting the last day of the month' do
+      Month.new(2014, 1).end_date.must_equal(Date.new(2014, 1, 31))
+    end
+  end
+
+  describe 'dates method' do
+    it 'returns the range of dates in the month' do
+      dates = Month.new(2014, 1).dates
+      dates.must_be_instance_of(Range)
+      dates.count.must_equal(31)
+      dates.all? { |date| Date === date }.must_equal(true)
+      dates.first.must_equal(Date.new(2014, 1, 1))
+      dates.last.must_equal(Date.new(2014, 1, 31))
+    end
+  end
+
+  describe 'length method' do
+    it 'returns the integer number of days in the month' do
+      Month.new(2014, 1).length.must_equal(31)
+      Month.new(2014, 2).length.must_equal(28)
+    end
+  end
+end
+
+describe 'Month parse method' do
+  it 'returns the month corresponding to the given string representation' do
+    Month.parse('2014-01').must_equal(Month.new(2014, 1))
+  end
+
+  it 'raises an exception if the format of the string is not as expected' do
+    proc { Month.parse('January 2014') }.must_raise(ArgumentError)
+  end
+end
+
+describe 'Month method' do
+  it 'returns the month of the given date object' do
+    Month(Date.new(2014, 1, 1)).must_equal(Month.new(2014, 1))
+  end
+
+  it 'returns the month of the given time object' do
+    Month(Time.at(1234567890)).must_equal(Month.new(2009, 2))
+  end
+
+  it 'returns the month of the given datetime object' do
+    Month(DateTime.parse('2001-02-03T04:05:06+07:00')).must_equal(Month.new(2001, 2))
+  end
+
+  it 'returns the month of the given integer unix timestamp' do
+    Month(1234567890).must_equal(Month.new(2009, 2))
+  end
+
+  it 'idempotently returns the given month' do
+    month = Month.new(2014, 1)
+
+    Month(month).object_id.must_equal(month.object_id)
+  end
+end
+
+describe 'A method defined in Month::Methods' do
+  include Month::Methods
+
+  it 'returns a month object denoting that month in the given year' do
+    month = January 2014
+    month.must_equal(Month.new(2014, 1))
+
+    month = February 2014
+    month.must_equal(Month.new(2014, 2))
+  end
+
+  it 'returns a date object when given two arguments' do
+    date = January 31, 2014
+    date.must_equal(Date.new(2014, 1, 31))
+
+    date = February 28, 2014
+    date.must_equal(Date.new(2014, 2, 28))
+  end
+
+  it 'raises an exception if given too many arguments' do
+    proc { January 1, 2, 3 }.must_raise(ArgumentError)
+  end
+end
